e190b8e29fb7b7de608c07a8396a9c84d7de1c03
automationator <automationator@gmail.com>
2017-03-02 10:48:06 -0500

    Made __ variables _ variables

--- integralutils/RegexHelpers.py
+++ integralutils/RegexHelpers.py
@@ -2,19 +2,19 @@ import re
 import base64
 import html
 
-__email_utf8_encoded_string = re.compile(r'.*(\=\?UTF\-8\?B\?(.*)\?=).*')
-__email_address = re.compile(r'[a-zA-Z0-9._%+\-"]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9_-]{2,}')
-__ip = re.compile(r'(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)')
-__domain = re.compile(r'(((?=[a-zA-Z0-9-]{1,63}\.)[a-zA-Z0-9]+(-[a-zA-Z0-9]+)*\.)+[a-zA-Z]{2,63})')
-__url = re.compile(r'(((?:(?:https?|ftp)://)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_:\?]*)#?(?:[\.\!\/\\\w:%\?&;=-]*))?(?<!=))')
-__md5 = re.compile(r'^[a-fA-F0-9]{32}$')
-__sha1 = re.compile(r'^[a-fA-F0-9]{40}$')
-__sha256 = re.compile(r'^[a-fA-F0-9]{64}$')
-__sha512 = re.compile(r'^[a-fA-F0-9]{128}$')
-__strings = re.compile(b'[^\x00-\x1F\x7F-\xFF]{4,}')
+_email_utf8_encoded_string = re.compile(r'.*(\=\?UTF\-8\?B\?(.*)\?=).*')
+_email_address = re.compile(r'[a-zA-Z0-9._%+\-"]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9_-]{2,}')
+_ip = re.compile(r'(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)')
+_domain = re.compile(r'(((?=[a-zA-Z0-9-]{1,63}\.)[a-zA-Z0-9]+(-[a-zA-Z0-9]+)*\.)+[a-zA-Z]{2,63})')
+_url = re.compile(r'(((?:(?:https?|ftp)://)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_:\?]*)#?(?:[\.\!\/\\\w:%\?&;=-]*))?(?<!=))')
+_md5 = re.compile(r'^[a-fA-F0-9]{32}$')
+_sha1 = re.compile(r'^[a-fA-F0-9]{40}$')
+_sha256 = re.compile(r'^[a-fA-F0-9]{64}$')
+_sha512 = re.compile(r'^[a-fA-F0-9]{128}$')
+_strings = re.compile(b'[^\x00-\x1F\x7F-\xFF]{4,}')
 
 def decode_utf_b64_string(value):
-    match = __email_utf8_encoded_string.match(value)
+    match = _email_utf8_encoded_string.match(value)
     
     if match:
         # Match the full encoded portion of the string. Ex: =?UTF-8?B?TsKqOTE4NzM4Lmh0bWw=?=
@@ -33,7 +33,7 @@ def decode_utf_b64_string(value):
         return value
 
 def find_urls(value):
-    urls = __url.findall(value)
+    urls = _url.findall(value)
     unescaped_urls = [html.unescape(url[0]) for url in urls]
     
     cleaned_urls = set()
@@ -60,41 +60,41 @@ def find_urls(value):
     return sorted(list(cleaned_urls))
     
 def find_strings(value):
-    matches = __strings.findall(value)
+    matches = _strings.findall(value)
     return [str(s, 'utf-8') for s in matches]
 
 def find_ip_addresses(value):
-    return __ip.findall(value)
+    return _ip.findall(value)
 
 def find_domains(value):
-    return __domain.findall(value)
+    return _domain.findall(value)
 
 def is_md5(value):
-    if __md5.match(value):
+    if _md5.match(value):
         return True
     else:
         return False
     
 def is_sha1(value):
-    if __sha1.match(value):
+    if _sha1.match(value):
         return True
     else:
         return False
     
 def is_sha256(value):
-    if __sha256.match(value):
+    if _sha256.match(value):
         return True
     else:
         return False
 
 def is_sha512(value):
-    if __sha512.match(value):
+    if _sha512.match(value):
         return True
     else:
         return False
 
 def is_ip(value):
-    if __ip.match(value):
+    if _ip.match(value):
         return True
     else:
         return False
