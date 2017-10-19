dc6eab4005489119005a8a16880c6bf158d3d9ce
automationator <automationator@gmail.com>
2017-10-19 16:10:01 -0400

    added is_domain_url_shortener

--- integralutils/Whitelist.py
+++ integralutils/Whitelist.py
@@ -51,6 +51,15 @@ class Whitelist():
     def __setstate__(self, d):
         self.__dict__.update(d)
 
+    def is_domain_url_shortener(self, domain):
+        if hasattr(self, "Whitelists_shortlinks"):
+            if domain in self.Whitelists_shortlinks:
+                return True
+            else:
+                return False
+        else:
+            return False
+
     def is_tor_node(self, ip):
         if hasattr(self, "Benignlists_tor_nodes"):
             if ip in self.Benignlists_tor_nodes:
